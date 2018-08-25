a0f643514890f868d01b45f41512c8d1495829a5
wesleyflorence <jw37g@mac.com>
2018-08-24 17:36:21 -0700

    initial commit

---
+++ freshheads.py
@@ -0,0 +1,19 @@
+import praw
+from reddit_credentials import client_id, client_secret, user_agent, username, password
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     username=username,
+                     password=password)
+
+multireddit = reddit.multireddit('freshheads', 'freshheads')
+
+print(20*'-')
+
+for submission in multireddit.hot(limit=200):
+  fresh = submission.title
+  if "FRESH" in fresh:
+    print(fresh)
+
+print(20*'-')
